39a82433d78f2db8f2e81cdb4f693084276bace3
ImAadarsh <aadarshkavita@gmail.com>
2025-05-08 00:37:16 +0530

    AA

--- faq.php
+++ faq.php
@@ -36,65 +36,7 @@
     <?php include 'include/header.php'; ?>
     <!-- header end  -->
 
-    <!-- tp header search  -->
-    <div class="tp-header-search-bar d-flex align-items-center">
-        <button class="tp-search-close">×</button>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6">
-                    <div class="tp-search-bar text-center">
-                        <h2 class="tp-search-bar-title mb-30">What are you looking for?</h2>
-                        <div class="contact-form-box contact-search-form-box">
-                            <form action="#">
-                                <input type="text" placeholder="Search Here">
-                                <button type="submit"><i class="far fa-search"></i></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="tp-offcanvas">
-        <div class="tp-offcanvas-wrapper">
-            <div class="tp-offcanvas-header d-flex justify-content-between align-items-center mb-90">
-                <div class="tp-offcanvas-logo">
-                    <a href="index.php"><img src="assets/img/logo/logo-black.png" alt=""></a>
-                </div>
-                <div class="tp-offcanvas-close">
-                    <button class="tp-offcanvas-close-toggle"><i class="fal fa-times"></i></button>
-                </div>
-            </div>
-            <div class="tp-offcanvas-menu d-xl-none mb-50">
-                <nav></nav>
-            </div>
-            <div class="tp-offcanvas-content mb-50 d-none d-xl-block">
-                <h2 class="tp-offcanvas-title">Hello There!</h2>
-                <p>Lorem ipsum dolor sit amet, consect etur adipiscing elit. </p>
-            </div>
-            <div class="tp-offcanvas-gallery mb-50">
-                <a class="popup-image" href="assets/img/project/showcase-thumb-01.jpg"><img src="assets/img/project/showcase-thumb-01.jpg" alt=""></a>
-                <a class="popup-image" href="assets/img/project/showcase-thumb-02.jpg"><img src="assets/img/project/showcase-thumb-02.jpg" alt=""></a>
-                <a class="popup-image" href="assets/img/project/showcase-thumb-03.jpg"><img src="assets/img/project/showcase-thumb-03.jpg" alt=""></a>
-                <a class="popup-image" href="assets/img/project/showcase-thumb-04.jpg"><img src="assets/img/project/showcase-thumb-04.jpg" alt=""></a>
-            </div>
-            <div class="tp-offcanvas-info mb-50">
-                <h3 class="tp-offcanvas-sm-title">Information</h3>
-                <span><a href="tel:01234567">+91 769700123</a></span>
-                <span><a href="mailto:aim@ipnacademy.in">aim@ipnacademy.in</a></span>
-                <span><a target="_blank" href="https://www.google.com/maps/">Avenue de Roma 158b, Lisboa</a></span>
-            </div>
-            <div class="tp-offcanvas-social mb-50">
-                <h3 class="tp-offcanvas-sm-title">Follow Us</h3>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-        </div>
-    </div>
-    <div class="tp-offcanvas-overlay"></div>
     
     <div id="smooth-wrapper">
         <div id="smooth-content">
--- include/sidebar.php
+++ include/sidebar.php
@@ -1,52 +1,5 @@
-<header>
-        <div id="tp-header-sticky" class="tp-header-area tp-transparent-header pl-100 tp-header-border tp-sticky-black">
-            <div class="container-fluid p-0">
-                <div class="row gx-0 align-items-center">
-                    <div class="col-xl-2 col-lg-5 col-md-4 col-6">
-                        <div class="tp-logo">
-                            <a href="index.php"><img src="assets/img/logo/logo-white.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="col-xl-10 col-lg-7 col-md-8 col-6">
-                        <div class="tp-header-box d-flex justify-content-xl-between justify-content-end">
-                            <div class="tp-main-menu pl-45 d-none d-xl-block">
-                                <nav class="tp-mobile-menu-active">
-                                    <ul>
-                                        <li><a href="index.php">Home</a></li>
-                                        <li><a href="about.php">About Us</a></li>
-                                        <li><a href="services.php">Services</a></li>
-                                        <li><a href="team.php">Team</a></li>
-                                        <li><a href="faq.php">FAQ</a></li>
-                                        <li><a href="contact.php">Contact</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <div class="tp-header-right d-flex justify-content-end flex-wrap align-items-center">
-                                <div class="tp-header-search d-none d-md-flex">
-                                    <button class="d-flex align-items-center tp-search-toggle"><i class="flaticon-search"></i></button>
-                                </div>
-                                <div class="tp-header-cta d-none d-md-flex">
-                                    <div class="tp-header-cta-icon d-flex align-items-center">
-                                        <i class="flaticon-phone-book"></i>
-                                    </div>
-                                    <div class="tp-header-cta-text">
-                                        <span>Free Call</span>
-                                        <a href="tel:0123456">+91 769700123 </a>
-                                    </div>
-                                </div>
-                                <div class="tp-header-bar">
-                                    <button class="tp-offcanvas-toogle"><i class="flaticon-menu"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-
-            <!-- tp header search  -->
-            <div class="tp-header-search-bar d-flex align-items-center">
+        <!-- tp header search  -->
+        <div class="tp-header-search-bar d-flex align-items-center">
             <button class="tp-search-close">×</button>
             <div class="container">
                 <div class="row justify-content-center">
